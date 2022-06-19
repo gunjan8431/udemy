6a903e8c14ed977183deab79ebad5f96098f8608
gunjank <Gunjan.Kathale@bitwiseglobal.com>
2022-06-19 13:02:51 +0530

    first commit

--- C/src/A.java
+++ C/src/A.java
@@ -1,7 +1,8 @@
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println(" eclipse 1gdfgdfgd push");
+		System.out.println(" eclipse ghnnhtnhtnhtnht1gdfgdfgd push");
+		System.out.println("jjrgjgjrj");
 	}
 
 }
