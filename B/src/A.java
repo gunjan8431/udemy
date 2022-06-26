dd31235d6976167f2d762cc9283b22adde6647a3
gunjan <gunjankathale7@gmail.com>
2022-06-26 10:27:16 +0530

    git commit

--- B/src/A.java
+++ B/src/A.java
@@ -1,7 +1,8 @@
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println(" eclipse 1 push");
+		System.out.println(" eclipse 2 push");
+		System.out.println("git push");
 	}
 
 }
