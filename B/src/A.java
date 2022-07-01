ccd4c792417e17e2d2bb2c5f35d660d63a9a383b
Shubham <Shubham@DESKTOP-QH14NDJ>
2022-07-01 16:30:58 +0530

    add syso

--- B/src/A.java
+++ B/src/A.java
@@ -3,6 +3,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println(" eclipse 2 push");
 		System.out.println("git push");
+		System.out.println("git pull request");
 	}
 
 }
